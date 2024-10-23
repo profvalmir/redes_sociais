b02609d4834a510a7c278e2022872b8dfab5f5a1
profvalmir <163314438+profvalmir@users.noreply.github.com>
2024-10-23 15:50:55 -0300

    Add files via upload

---
+++ informacoes/common.js
@@ -0,0 +1,16 @@
+
+
+
+
+const getCSS = (variavel) => {
+    return getComputedStyle(document.body).getPropertyValue(variavel)
+  }
+  
+  const tickConfig = {
+    color: getCSS('--primary-color'),
+    size: 16,
+    family: getCSS('--font')
+  }
+  
+  export { getCSS, tickConfig }
+  
--- informacoes/usuarios.js
+++ informacoes/usuarios.js
@@ -1,4 +1,9 @@
 
+
+import { getCSS, tickConfig } from "./common.js"
+
+
+
 async function quantidadeUsuarios() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/refs/heads/main/numero-usuarios.json'
     const res = await fetch(url)
@@ -10,15 +15,50 @@ async function quantidadeUsuarios() {
     {
       x: nomeDasRedes,
       y: quantidadeUsuarios,
-      type: 'bar'
+      type: 'bar',
+      marker: {
+        color: getCSS('--primary-color')
+      }
     }
   ]
   
+  const layout = {
+    plot_bgcolor: getCSS('--bg-color'),
+    paper_bgcolor: getCSS('--bg-color'),
+    title: {
+      text: 'Redes sociais com mais usuários no mundo',
+       x: 0,
+       font: {
+           color: getCSS('--primary-color'),
+           family: getCSS('--font'),
+           size: 30
+       }
+    },
+    xaxis: {
+      tickfont: tickConfig,
+      title: {
+          text: 'Nome das redes sociais',
+          font: {
+              color: getCSS('--secondary-color')
+          }
+      }
+  },
+  yaxis: {
+    tickfont: tickConfig,
+      title: {
+          text: 'Bilhões de usuários ativos',
+          font: {
+              color: getCSS('--secondary-color')
+          }
+      }
+  }
+  }
+
+
   const grafico = document.createElement('div')
   grafico.className = 'grafico'
   document.getElementById('graficos-container').appendChild(grafico)
-  Plotly.newPlot(grafico, data)
-  
+  Plotly.newPlot(grafico, data, layout)
   }
   
   quantidadeUsuarios()
